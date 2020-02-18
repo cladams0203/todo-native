beb8cd01de58e61b6fcf9995ae55c668a2066786
cladams0203 <cladams0203@gmail.com>
2020-02-18 13:17:15 -0500

    added native file structure

---
+++ components/TodoList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export function TodoList() {
+
+    return (
+        <View>
+            <Text>Hello</Text>
+        </View>
+    )
+}
